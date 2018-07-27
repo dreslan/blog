2780c3a9d72ecb8cdfe0bddc756b69e889d54a5a
acoppola <anthony.coppola@turner.com>
2018-07-26 23:45:06 -0400

    Built list and detail views. Created templates for views. Changed list view to class-based view. Add CSS. Added pagination.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -16,8 +21,17 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-class Meta:
-    ordering = ('-publish',)
+    objects = models.Manager() # The default manager
+    published = PublishedManager() # The Dahl-specific manager
 
-def __str__(self):
-    return self.title
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, 
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
